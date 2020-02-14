ab2bc3b82b0c0f9b4573a8dd1758f784e1315c4f
Treinar <Treinar@MICRO60>
2020-02-14 20:12:36 -0200

    Atualizado

--- pabxSankhya/src/br/com/dvl/integraSankhya/EscutaPorta.java
+++ pabxSankhya/src/br/com/dvl/integraSankhya/EscutaPorta.java
@@ -1,8 +1,8 @@
 package br.com.dvl.integraSankhya;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,20 +15,29 @@ public class EscutaPorta {
 		server = new ServerSocket(port);
 		while (true) {
 			Socket socket = server.accept();
-			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-			String message = (String) ois.readObject();
-			System.out.println("Message Received: " + message);
-			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-			oos.writeObject("Hi Client " + message);
-			ois.close();
-			oos.close();
-			socket.close();
-			if (message.equalsIgnoreCase("exit"))
+			System.out.println(socket.getInputStream());
+			BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+            	System.out.println(line);
+            }
+//			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+//			String message = (String) ois.readObject();
+//			System.out.println("Message Received: " + message);
+//			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+//			oos.writeObject("Hi Client " + message);
+//			ois.close();
+//			oos.close();
+//			socket.close();
+			if (/*message.equalsIgnoreCase("exit")*/"".equals("a")) {
 				break;
+			}
 		}
-		System.out.println("Shutting down Socket server!!");
+//		System.out.println("Shutting down Socket server!!");
 		// close the ServerSocket object
 		server.close();
 	}
 
 }
+//https://www.programcreek.com/java-api-examples/?class=java.net.Socket&method=getInputStream
